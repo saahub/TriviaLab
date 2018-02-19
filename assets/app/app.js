abe125ef248da2615dc715f3417e064a92e89991
saahub <carolina-saavedra1@hotmail.com>
2018-02-19 14:33:13 -0300

    actualizo

--- assets/app/app.js
+++ assets/app/app.js
@@ -1,43 +1,60 @@
 const btonStart = document.getElementById('botn');
-const btonTrue = document.getElementById('true');
-const btonFalse = document.getElementById('false');
+
+const here = document.getElementById('here');
+
 
 btonStart.addEventListener('click', function(event){
 getCero();
 })
 
-btonTrue.addEventListener('click', function(event){
-tF();
-})
 
-btonFalse.addEventListener('click', function(event){
-tf();
-})
 
 function getCero() {
-  fetch(`https://opentdb.com/api.php?amount=10`)
+  fetch(`https://opentdb.com/api.php?amount=10&type=boolean`)
     .then(function(response) {
     // Turns the the JSON into a JS object
       return response.json();
     })
     .then(function(data) {
       console.log(data);
+          let i = 0;
+    let question = data.results[i].question;
+    let difficulty = data.results[i].difficulty;
+    let correctAnswer = data.results[i].correct_answer;
+    let wrongAnswer = data.results[i].incorrect_answers[i];
       let html = `<div class="container-fluid">
       <div class="row">
       <div class="col s12 l12">
         <p>${data.results[0].question}</p>
-        <a class="waves-effect waves-light btn-large purple accent-2" id="btonTrue" ${data.results[0].correct_answer} onclick=tf();>True</a>
-        <a class="waves-effect waves-light btn-large purple accent-2" id="btonFalse" ${data.results[0].incorrect_answer} onclick=tf();>False</a>
+        <a class="waves-effect waves-light btn-large purple accent-2" id="btonTrue">True</a>
+        <a class="waves-effect waves-light btn-large purple accent-2" id="btonFalse">False</a>
         </div>`;
     //Put that HTML on the page
-  document.getElementById('here').innerHTML = html;
-  })
+  here.innerHTML = html;
+  const btonFalse = document.getElementById('btonFalse');
+  const btonTrue = document.getElementById('btonTrue');
+
+
+  btonTrue.addEventListener('click', function(event){
+    if(correctAnswer === 'false') {
+      alert('correcto')
+    }
+    else {
+      alert('noooo!')
+    }
+
+})
+
+btonFalse.addEventListener('click', function(event){
+
+})
+  });
     botn.remove();
     out.remove();
-  }
+  };
 
 
- function tF() {
+/* function tF() {
   fetch(`https://opentdb.com/api.php?amount=10&type=boolean`)
   .then(function(response){
     return response.json();
@@ -50,4 +67,4 @@ function getCero() {
     let correctAnswer = results[i].correct_answer;
     let wrongAnswer = results[i].incorrect_answers[i];
   });
-}
+}*/
